b10882b76933450a9d754d78c43278678d9ab7a0
davidwu <davidwu@nvidai.com>
2020-02-25 03:28:26 +0000

    Modify MultiplyLayer wgrad computation to +=

--- HugeCTR/include/layers/multiply_layer.hpp
+++ HugeCTR/include/layers/multiply_layer.hpp
@@ -58,8 +58,8 @@ class MultiplyLayer : public Layer {
 
  private:
 
-  Tensors<float> wgrad_tmp_trans_;
-
+  std::shared_ptr<GeneralBuffer<float>> internal_buff_;
+  std::unique_ptr<Tensor<float>> wgrad_tmp_trans_;
 };
 
 }  // namespace HugeCTR
