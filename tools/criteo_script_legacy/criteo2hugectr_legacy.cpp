17b8128e31dd279c92a7947deb5f870588b83dab
zehuanw <zehuanw@nvidia.com>
2020-02-27 08:22:40 -0800

    add new criteo2hugectr

--- tools/criteo_script_legacy/criteo2hugectr_legacy.cpp
+++ tools/criteo_script_legacy/criteo2hugectr_legacy.cpp
@@ -24,14 +24,13 @@
 
 using namespace HugeCTR;
 
-static std::string usage_str = "usage: ./criteo2hugectr in.txt dir/prefix file_list.txt"; 
+static std::string usage_str = "usage: ./criteo2hugectr_legacy <num slots> in.txt dir/prefix file_list.txt"; 
 
 static const int N = 40960; //number of samples per data file
-static const int KEYS_PER_SAMPLE = 26;
-static const int dense_dim = 13;
+static const int KEYS_PER_SAMPLE = 39;
 typedef long long T;
 static const long long label_dim = 1;
-static const int SLOT_NUM = 26;
+static const int voc_size = 1603616;
 
 
 std::vector<std::string> &split(const std::string &s, char delim, std::vector<std::string> &elems) {
@@ -48,17 +47,18 @@ int main(int argc, char* argv[]){
 
   const std::string tmp_file_list_name("file_list.tmp");
 
-  if (argc != 4){
+  if (argc != 5){
     std::cout << usage_str << std::endl;
     exit(-1);
   }
+  const int SLOT_NUM = atoi(argv[1]);
   //open txt file
-  std::ifstream txt_file(argv[1], std::ifstream::binary);
+  std::ifstream txt_file(argv[2], std::ifstream::binary);
   if(!txt_file.is_open()){
-    std::cerr << "Cannot open argv[1]" << std::endl;
+    std::cerr << "Cannot open argv[2]" << std::endl;
   }
   //create a data file under prefix
-  std::string data_prefix(argv[2]);
+  std::string data_prefix(argv[3]);
   std::string directory;
   const size_t last_slash_idx = data_prefix.rfind('/');
   if (std::string::npos != last_slash_idx){
@@ -81,7 +81,7 @@ int main(int argc, char* argv[]){
     }
 
     DataWriter<Check_t::Sum> data_writer(data_file);
-    DataSetHeader header = {1, static_cast<long long>(N), label_dim, dense_dim, static_cast<long long>(SLOT_NUM), 0, 0, 0
+    DataSetHeader header = {1, static_cast<long long>(N), label_dim, 0, static_cast<long long>(SLOT_NUM), 0, 0, 0
                             };
     data_writer.append(reinterpret_cast<char*>(&header), sizeof(DataSetHeader));
     data_writer.write();
@@ -95,7 +95,7 @@ int main(int argc, char* argv[]){
       if(txt_file.eof()){
         txt_file.close();
         data_file.seekp(std::ios_base::beg);
-        DataSetHeader last_header = {1, static_cast<long long>(i), label_dim, dense_dim,
+        DataSetHeader last_header = {1, static_cast<long long>(i), label_dim, 0,
                                      static_cast<long long>(SLOT_NUM), 0, 0, 0};
 	data_writer.append(reinterpret_cast<char*>(&last_header), sizeof(DataSetHeader));
 	data_writer.write();
@@ -108,14 +108,14 @@ int main(int argc, char* argv[]){
             std::cerr << "Cannot open " << tmp_file_list_name << std::endl;
           }
 
-          std::cout << "Opening " << argv[3] << std::endl;
-          std::ofstream file_list(argv[3], std::ofstream::out);
+          std::cout << "Opening " << argv[4] << std::endl;
+          std::ofstream file_list(argv[4], std::ofstream::out);
           if(!file_list.is_open()){
-            std::cerr << "Cannot open " << argv[3] << std::endl;
+            std::cerr << "Cannot open " << argv[4] << std::endl;
           }
 
           
-          file_list << (std::to_string(file_counter+1) + "\n");
+          file_list << (std::to_string(file_counter) + "\n");
           file_list << tmp.rdbuf();
 
           tmp.close();
@@ -126,33 +126,33 @@ int main(int argc, char* argv[]){
       }
       std::vector<std::string> vec_string;
       split(line, ' ', vec_string);
-      if(vec_string.size() != KEYS_PER_SAMPLE+dense_dim+label_dim) //first one is label
+      if(vec_string.size() != KEYS_PER_SAMPLE+1) //first one is label
         {
-          std::cerr << "vec_string.size() != KEYS_PER_SAMPLE+dense_dim+label_dim" << std::endl;
+          std::cerr << "vec_string.size() != KEYS_PER_SAMPLE+1" << std::endl;
           std::cerr << line << std::endl;
           exit(-1);
         }
+      float label = std::stoi(vec_string[0]);
 #ifndef NDEBUG
       std::cout << std::endl;
+      std::cout << label << ' ';
 #endif
-      for(int j = 0; j < dense_dim+label_dim; j++){
-	float label_dense = std::stod(vec_string[j]);
-	data_writer.append(reinterpret_cast<char*>(&label_dense), sizeof(float));
-#ifndef NDEBUG
-      std::cout << label_dense << ' ';
-#endif
+      data_writer.append(reinterpret_cast<char*>(&label), sizeof(float));
+      std::vector<T> slots[SLOT_NUM];
+      for(int j = 0; j < KEYS_PER_SAMPLE; j++){
+        T key = static_cast<T>(std::stoi(vec_string[j+1]));
+        int slot_id = key%SLOT_NUM;
+        slots[slot_id].push_back(key);
       }
-
-      int nnz = 1;
-      for(int j = dense_dim+label_dim; j < KEYS_PER_SAMPLE+dense_dim+label_dim; j++){
-        T key = static_cast<T>(std::stoi(vec_string[j]));
+      for(int j = 0; j < SLOT_NUM; j++){
+        int nnz = slots[j].size();
 	data_writer.append(reinterpret_cast<char*>(&nnz), sizeof(int));
-	data_writer.append(reinterpret_cast<char*>(&key), sizeof(T));
-
+        for(T key : slots[j]){
+	  data_writer.append(reinterpret_cast<char*>(&key), sizeof(T));
 #ifndef NDEBUG
-	std::cout << key << ',';
+          std::cout << j << ',' << key << ' ';
 #endif
-
+        }
       }
       data_writer.write();
     }
---
+++ tools/criteo_script_legacy/usage.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+#decompresion
+tar zxvf dac.tar.gz
+
+head -n 36672493 train.txt > train
+tail -n 9168124 train.txt > valtest
+head -n 4584062 valtest > val
+tail -n 4584062 valtest > test
+
+# will produce train.out val.out test.out
+./preprocess.pl train val test
+
+
+# may need to shuffle train.out & val.out
