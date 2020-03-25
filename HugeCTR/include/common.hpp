f98c06c8ff0aa17d77e581d486f4c03264984300
zehuanw <zehuanw@nvidia.com>
2020-03-24 21:58:35 -0700

    update license and version on all the files

--- HugeCTR/include/common.hpp
+++ HugeCTR/include/common.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019, NVIDIA CORPORATION.
+ * Copyright (c) 2020, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -30,7 +30,7 @@
 namespace HugeCTR {
 
 #define HUGECTR_VERSION_MAJOR 2
-#define HUGECTR_VERSION_MINOR 0
+#define HUGECTR_VERSION_MINOR 1
 
 #define WARP_SIZE 32
 
