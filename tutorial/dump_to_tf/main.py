7380d41d312c3efb835b4d471414425895f934d4
davidwu <davidwu@nvidia.com>
2020-05-11 04:45:48 -0400

    Add Criteo Model

--- tutorial/dump_to_tf/main.py
+++ tutorial/dump_to_tf/main.py
@@ -7,7 +7,7 @@ from hugectr_layers import *
 import argparse
 
 
-def read_a_sample(args, slot_num=26):
+def read_a_sample_for_dcn(args, slot_num=26):
     """
     read a sample from criteo dataset.
     """
@@ -66,7 +66,10 @@ def dcn_model(args):
                          model_json = model_json,
                          non_training_params_json = None)
 
-    checkpoint = r'./tf_checkpoint/dcn_model'
+    checkpoint_path = r'./tf_checkpoint/dcn/'
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    checkpoint = os.path.join(checkpoint_path, "dcn_model")
 
     #----------------- build computing graph--------------------#
     tf.reset_default_graph()
@@ -163,10 +166,164 @@ def dcn_model(args):
         sess.run(init_op)
 
         # check inference output
-        label, dense, keys = read_a_sample(args)
+        label, dense, keys = read_a_sample_for_dcn(args)
         keys[keys == -1] = vocabulary_size # map -1 to invalid zeros embedding feature
         output = sess.run(fc4, feed_dict={dense_input: dense,
                                           sparse_input: keys})
+        print("[INFO] output = %f" %output)
+
+        # save checkpoint
+        saver.save(sess, checkpoint, global_step=0)
+        print("[INFO] save done.")
+
+
+def read_a_sample_for_criteo(args, slot_num=1):
+    """
+    read a sample from criteo dataset
+    """
+    with open(os.path.join(args.dataset_path, "sparse_embedding0.data"), 'rb') as file:
+        # skip data_header
+        file.seek(4 + 64 + 1, 0)
+
+        # one sample
+        length_buffer = file.read(4) # int
+        length = struct.unpack("i", length_buffer)[0]
+
+        label_buffer = file.read(4) # int
+        label = struct.unpack("i", label_buffer)[0]
+
+        # no dense
+
+        keys = []
+        for _ in range(slot_num):
+            nnz_buffer = file.read(4) # int
+            nnz = struct.unpack("i", nnz_buffer)[0]
+            keys_buffer = file.read(8 * nnz) # nnz * long long
+            key = struct.unpack(str(nnz) + "q", keys_buffer)
+            keys += list(key)
+            
+        check_bit_buffer = file.read(1) # char
+        check_bit = struct.unpack("c", check_bit_buffer)
+
+    label = np.int64(label)
+    dense = []
+    keys = np.reshape(np.array(keys, dtype=np.int64), newshape=(1, 1, 39)) #[batch, slot_num, nnz]
+
+    return label, dense, keys
+
+
+def criteo_model(args):
+    """
+    this function build "criteo" computing-graph with tf.
+    and initialize the weights with values dumped from
+    hugectr.
+    """
+    batchsize = 1
+    slot_num = 1
+    max_nnz_per_slot = 39
+
+    dense_dim = 0
+
+    samples_dir = r'../../samples/'
+    model_json = os.path.join(samples_dir, r'criteo/criteo.json')
+
+    model_path = args.model_file_path
+    sparse_model_names = [os.path.join(model_path, r'criteo_model0_sparse_40000.model')]
+    dense_model_name = os.path.join(model_path, r'criteo_model_dense_40000.model')
+
+    dump = DumpToTF(sparse_model_names = sparse_model_names,
+                         dense_model_name = dense_model_name,
+                         model_json = model_json,
+                         non_training_params_json = None)
+
+    checkpoint_path = r'./tf_checkpoint/criteo/'
+    if not os.path.exists(checkpoint_path):
+        os.makedirs(checkpoint_path)
+    checkpoint = os.path.join(checkpoint_path, "criteo_model")
+        
+
+    #----------------- build computing graph--------------------#
+    tf.reset_default_graph()
+
+    graph = tf.Graph()
+    with graph.as_default():
+        # no dense input
+
+        # sparse input = [batchsize, slot_num, max_nnz_per_slot]
+        sparse_input = tf.placeholder(shape=(batchsize, slot_num, max_nnz_per_slot),
+                            dtype=tf.int64, name='sparse-input')
+
+        # dump embedding to tf
+        layer_name, init_values = dump.parse_embedding().__next__()
+        vocabulary_size = init_values.shape[0]
+        embedding_feature = embedding_layer(sparse_input, init_values, combiner=0)
+        
+        # reshape1
+        leading_dim = 64
+        reshape1 = tf.reshape(embedding_feature, [-1, leading_dim])
+
+        # dump fc1 to tf
+        layer_type = "InnerProduct"
+        num_output = 200
+        layer_bytes = (reshape1.shape[1] * num_output + 1 * num_output) * 4
+        weight_fc1, bias_fc1 = dump.parse_dense(layer_bytes, layer_type,
+                                                in_feature=reshape1.shape[1],
+                                                out_feature=num_output)
+        fc1 = innerproduct_layer(reshape1, weight_fc1, bias_fc1)
+
+        # relu
+        relu1 = tf.nn.relu(fc1)
+
+        # dump fc2 to tf
+        layer_type = "InnerProduct"
+        num_output = 200
+        layer_bytes = (relu1.shape[1] * num_output + 1 * num_output) * 4
+        weight_fc2, bias_fc2 = dump.parse_dense(layer_bytes, layer_type,
+                                                in_feature=relu1.shape[1],
+                                                out_feature=num_output)
+        fc2 = innerproduct_layer(relu1, weight_fc2, bias_fc2)
+
+        # relu2
+        relu2 = tf.nn.relu(fc2)
+
+        # dump fc3 to tf
+        layer_type = "InnerProduct"
+        num_output = 200
+        layer_bytes = (relu2.shape[1] * num_output + 1 * num_output) * 4
+        weight_fc3, bias_fc3 = dump.parse_dense(layer_bytes, layer_type,
+                                                in_feature=relu2.shape[1],
+                                                out_feature=num_output)
+        fc3 = innerproduct_layer(relu2, weight_fc3, bias_fc3)
+
+        # relu3
+        relu3 = tf.nn.relu(fc3)
+
+        # dump fc4 to tf
+        layer_type = "InnerProduct"
+        num_output = 1
+        layer_bytes = (relu3.shape[1] * num_output + 1 * num_output) * 4
+        weight_fc4, bias_fc4 = dump.parse_dense(layer_bytes, layer_type,
+                                                in_feature=relu3.shape[1],
+                                                out_feature=num_output)
+        fc4 = innerproduct_layer(relu3, weight_fc4, bias_fc4)
+
+        # check whether all dense weights are parsed
+        dump.read_dense_complete()
+
+        init_op = tf.group(tf.local_variables_initializer(),
+                            tf.global_variables_initializer())
+
+        saver = tf.train.Saver()
+
+    with tf.Session(graph=graph) as sess:
+        sess.graph.finalize()
+
+        sess.run(init_op)
+
+        # check inference output
+        label, dense, keys = read_a_sample_for_criteo(args)
+        keys[keys == -1] = vocabulary_size # map -1 to invalid zeros embedding feature
+        output = sess.run(fc4, feed_dict={sparse_input: keys})
         print("[INFO] output = %f" %output)
 
         # save checkpoint
@@ -180,6 +337,16 @@ if __name__ == "__main__":
                         help="where to find criteo dataset.")
     parser.add_argument("model_file_path", type=str, 
                         help="where to find model files.")
+    parser.add_argument("--model", "-m", type=str, default="dcn", 
+                        help="decide to run what model, must be 'dcn' or 'criteo'.")
+                    
     args = parser.parse_args()
 
-    dcn_model(args)
+    if args.model == 'dcn':
+        print("[INFO] running dcn...")
+        dcn_model(args)
+    elif args.model == 'criteo':
+        print("[INFO] running criteo...")
+        criteo_model(args)
+    else:
+        raise ValueError("You must use dcn or criteo model.")
