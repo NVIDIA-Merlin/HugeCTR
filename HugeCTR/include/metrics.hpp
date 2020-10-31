3c49da0bb24d115e7f7d13448eb848011b13786d
Dmitry Alexeev <dalexeev@nvidia.com>
2020-10-31 01:24:09 -0700

    Initial metric utest
    AUC utest: fixed fp16 and multi-gpu support
    
    wip: initial AUC implementation, passing small test
    
    fixed device_id to local_id mapping
    
    minor, easier debugging
    
    avoid full rebuilding when changing py file
    
    support arbitrary number of samples, non-sequential gpus and fp16
    
    use NCCL_LAUNCH_MODE=PARALLEL in CI for new AUC
    
    clamp predictions to range for more robust histograms
    
    trying to prevent a hang in all2all
    
    fixing naming typo
    
    AUC MPI test now working and passing
    
    fix pivots computation in the case of very uneven number of samples
    
    python2 utest compatibility
    
    drop MPI IO since it seems broken on the multi-node machine
    
    fix pointer in MPI utest

--- HugeCTR/include/metrics.hpp
+++ HugeCTR/include/metrics.hpp
@@ -23,13 +23,16 @@
 #include <tensor2.hpp>
 #include <utils.hpp>
 #include <vector>
+#include <mutex>
+#include <condition_variable>
 
 namespace HugeCTR {
 
 namespace metrics {
 
-enum class RawType { Loss = 0, Pred = 1, Label = 2 };
-enum class Type { AUC = 0, AverageLoss = 1 };
+using CountType = u_int32_t;
+enum class RawType { Loss, Pred, Label };
+enum class Type { AUC, AverageLoss };
 
 using RawMetricMap = std::map<RawType, TensorBag2>;
 
@@ -72,6 +75,100 @@ class AverageLoss : public Metric {
   int n_batches_;
 };
 
+class AUCStorage {
+  public:
+    float*      d_preds()                  const { return ptr_preds_1_                ; }
+    float*      d_labels()                 const { return ptr_labels_1_               ; }
+    CountType*  d_local_bins()             const { return ptr_local_bins_             ; }
+    CountType*  d_global_bins()            const { return ptr_global_bins_            ; }
+    CountType*  d_global_bins_sum()        const { return ptr_global_bins_sum_        ; }
+    CountType*  d_local_bins_sum()         const { return ptr_local_bins_sum_         ; }
+    int*        d_pivots()                 const { return ptr_pivots_                 ; }
+    CountType*  d_partition_offsets()      const { return ptr_partition_offsets_      ; }
+    float*      d_partitioned_preds()      const { return ptr_preds_2_                ; }
+    float*      d_partitioned_labels()     const { return ptr_labels_2_               ; }
+    CountType*  d_all_partition_offsets()  const { return ptr_all_partition_offsets_  ; }
+    CountType*  d_recv_offsets()           const { return ptr_recv_offsets_           ; }
+    float*      d_presorted_preds()        const { return ptr_preds_1_                ; }
+    float*      d_presorted_labels()       const { return ptr_labels_1_               ; }
+    float*      d_sorted_preds()           const { return ptr_preds_2_                ; }
+    float*      d_sorted_labels()          const { return ptr_labels_2_               ; }
+    float*      d_tp()                     const { return ptr_preds_1_                ; }
+    float*      d_fp()                     const { return ptr_labels_1_               ; }
+    float*      d_pos_per_gpu()            const { return ptr_pos_per_gpu_            ; }
+    float*      d_neg_per_gpu()            const { return ptr_neg_per_gpu_            ; }
+    float*      d_tpr()                    const { return ptr_preds_2_                ; }
+    float*      d_fpr()                    const { return ptr_labels_2_               ; }
+    CountType*  d_identical_pred_starts()  const { return ptr_identical_pred_starts_  ; }
+    CountType*  d_identical_pred_lengths() const { return ptr_identical_pred_lengths_ ; }
+    int*        d_num_identical_segments() const { return ptr_num_identical_segments_ ; }
+    float*      d_halo_tpr()               const { return ptr_halo_tpr_               ; }
+    float*      d_halo_fpr()               const { return ptr_halo_fpr_               ; }
+    CountType*  d_tp_offsets()             const { return ptr_tp_offsets_             ; }
+    CountType*  d_fp_offsets()             const { return ptr_fp_offsets_             ; }
+    float*      d_auc()                    const { return ptr_auc_                    ; }
+
+
+    void*       d_workspace()              const { return workspace_                  ; }
+
+    size_t& temp_storage_bytes() { return temp_storage_bytes_; }
+
+
+    void alloc_main(size_t num_local_samples, size_t num_bins, size_t num_partitions,
+                    size_t num_global_gpus);
+    void realloc_redistributed(size_t num_redistributed_samples);
+    void set_max_temp_storage_bytes(size_t new_val);
+    void alloc_workspace();
+    void free_all();
+
+  private:
+    const float imbalance_factor_ = 1.2f;
+    size_t temp_storage_bytes_ = 0;
+    size_t num_allocated_redistributed_ = 0;
+
+    float*     ptr_preds_1_                = nullptr;
+    float*     ptr_labels_1_               = nullptr;
+    float*     ptr_preds_2_                = nullptr;
+    float*     ptr_labels_2_               = nullptr;
+    CountType* ptr_local_bins_             = nullptr;
+    CountType* ptr_global_bins_            = nullptr;
+    CountType* ptr_global_bins_sum_        = nullptr;
+    CountType* ptr_local_bins_sum_         = nullptr;
+    int*       ptr_pivots_                 = nullptr;
+    CountType* ptr_partition_offsets_      = nullptr;
+    CountType* ptr_all_partition_offsets_  = nullptr;
+    CountType* ptr_recv_offsets_           = nullptr;
+    float*     ptr_pos_per_gpu_            = nullptr;
+    float*     ptr_neg_per_gpu_            = nullptr;
+    CountType* ptr_identical_pred_starts_  = nullptr;
+    CountType* ptr_identical_pred_lengths_ = nullptr;
+    int*       ptr_num_identical_segments_ = nullptr;
+    float*     ptr_halo_tpr_               = nullptr;
+    float*     ptr_halo_fpr_               = nullptr;
+    CountType* ptr_tp_offsets_             = nullptr;
+    CountType* ptr_fp_offsets_             = nullptr;
+    float*     ptr_auc_                    = nullptr;
+
+    void*      workspace_                  = nullptr;
+
+    void free_redistributed();
+};
+
+class AUCBarrier {
+public:
+    AUCBarrier(std::size_t thread_count);
+    void wait();
+
+private:
+    std::mutex mutex_;
+    std::condition_variable cond_;
+    std::size_t threshold_;
+    std::size_t count_;
+    std::size_t generation_;
+};
+
+
+
 template <typename T>
 class AUC : public Metric {
  public:
@@ -86,33 +183,28 @@ class AUC : public Metric {
   float finalize_metric() override;
   std::string name() const override { return "AUC"; };
 
- private:
-  void set_max_temp_storage_bytes(size_t& new_val);
-  float* d_pred() const { return (float*)temp0_; }
-  float* d_label() const { return (float*)temp1_; }
-  float* d_pred_sort() const { return (float*)temp2_; }
-  float* d_label_sort() const { return (float*)temp3_; }
-  float* d_inc_sum() const { return (float*)temp0_; }
-  float* tpr() const { return (float*)temp2_; }
-  int* d_flag_inc_sum() const { return (int*)temp0_; }
-  int* d_unique_index() const { return (int*)temp1_; }
-  float* fpr() const { return (float*)temp3_; }
-  float* d_auc() const { return (float*)temp0_; }
+  // Public in order to use device lambda
+  float _finalize_metric_per_gpu(int device_id);
 
-  void num_active_gpu_and_r(int& num_active_gpu, int& r);
+ private:
+  const float pred_min_ = 0.0f;
+  const float pred_max_ = 1.0f;
+  const int num_bins_per_gpu_ = 10000;
 
   std::shared_ptr<ResourceManager> resource_manager_;
-  int batch_size_per_gpu_;
+
   int n_batches_;
-  int root_device_id_;
-  int num_gpus_;
-  int offset_;
-  void* temp0_;
-  void* temp1_;
-  void* temp2_;
-  void* temp3_;
-  void* workspace_;
-  size_t temp_storage_bytes_;
+  int num_local_gpus_;
+  int num_global_gpus_;
+  int batch_size_per_gpu_;
+  int num_bins_;
+  int num_partitions_;
+  size_t num_total_samples_;
+
+  AUCBarrier barrier_;
+
+  std::vector<size_t> offsets_;
+  std::vector<AUCStorage> storage_;
 };
 
 }  // namespace metrics
