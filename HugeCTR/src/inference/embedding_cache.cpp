8b1c0d67223c0b3e327f071523c32aa7ebd9bfec
Yingcan Wei <yingcanw@nvidia.com>
2020-12-08 01:17:08 -0800

    Inference design interface

---
+++ HugeCTR/src/inference/embedding_cache.cpp
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2020, NVIDIA CORPORATION.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "HugeCTR/include/inference/embedding_cache.hpp"
+
+#include <iostream>
+#include <vector>
+namespace HugeCTR {
+template <typename T>
+embedding_cache<T>::embedding_cache(std::string model_name) {
+  model_name = model_name;
+}
+template <typename T>
+embedding_cache<T>::~embedding_cache() {}
+template <typename T>
+void embedding_cache<T>::look_up(T* embeddingcolumns, T* length, float* embeddingoutputvector) {}
+
+}  // namespace HugeCTR
