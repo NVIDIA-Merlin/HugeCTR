d13c912cd80f6ace3aaf1d19f57f210b3b672dbe
aleliu <aleliu@nvidia.com>
2020-12-15 19:03:24 +0800

    sperate get_optimizer_param

--- HugeCTR/src/parser.cpp
+++ HugeCTR/src/parser.cpp
@@ -128,76 +128,6 @@ static void add_tensor_to_network(TensorPair& output_tensor_pair,
       {output_tensor_pair.name, TensorUse::General, output_tensor_pair.tensor});
 }
 
-template <typename Type>
-static OptParams<Type> get_optimizer_param(const nlohmann::json& j_optimizer) {
-  // create optimizer
-  auto optimizer_name = get_value_from_json<std::string>(j_optimizer, "type");
-  Optimizer_t optimizer_type;
-  if (!find_item_in_map(optimizer_type, optimizer_name, OPTIMIZER_TYPE_MAP)) {
-    CK_THROW_(Error_t::WrongInput, "No such optimizer: " + optimizer_name);
-  }
-
-  OptHyperParams<Type> opt_hyper_params;
-  memset(&opt_hyper_params, 0, sizeof(opt_hyper_params));
-  OptParams<Type> opt_params;
-
-  Update_t update_type = Update_t::Local;
-  if (has_key_(j_optimizer, "update_type")) {
-    std::string update_name = get_value_from_json<std::string>(j_optimizer, "update_type");
-    if (!find_item_in_map(update_type, update_name, UPDATE_TYPE_MAP)) {
-      CK_THROW_(Error_t::WrongInput, "No such update type: " + update_name);
-    }
-  } else if (has_key_(j_optimizer, "global_update")) {
-    bool global_update = get_value_from_json<bool>(j_optimizer, "global_update");
-    if (global_update) update_type = Update_t::Global;
-  } else {
-    MESSAGE_("update_type is not specified, using default: local");
-  }
-
-  switch (optimizer_type) {
-    case Optimizer_t::Adam: {
-      auto j_hparam = get_json(j_optimizer, "adam_hparam");
-      float learning_rate = get_value_from_json<float>(j_hparam, "learning_rate");
-      float beta1 = get_value_from_json<float>(j_hparam, "beta1");
-      float beta2 = get_value_from_json<float>(j_hparam, "beta2");
-      float epsilon = get_value_from_json<float>(j_hparam, "epsilon");
-      opt_hyper_params.adam.beta1 = beta1;
-      opt_hyper_params.adam.beta2 = beta2;
-      opt_hyper_params.adam.epsilon = epsilon;
-      opt_params = {Optimizer_t::Adam, learning_rate, opt_hyper_params, update_type};
-      break;
-    }
-    case Optimizer_t::MomentumSGD: {
-      auto j_hparam = get_json(j_optimizer, "momentum_sgd_hparam");
-      float learning_rate = get_value_from_json<float>(j_hparam, "learning_rate");
-      float momentum_factor = get_value_from_json<float>(j_hparam, "momentum_factor");
-      opt_hyper_params.momentum.factor = momentum_factor;
-      opt_params = {Optimizer_t::MomentumSGD, learning_rate, opt_hyper_params, update_type};
-      break;
-    }
-    case Optimizer_t::Nesterov: {
-      auto j_hparam = get_json(j_optimizer, "nesterov_hparam");
-      float learning_rate = get_value_from_json<float>(j_hparam, "learning_rate");
-      float momentum_factor = get_value_from_json<float>(j_hparam, "momentum_factor");
-      opt_hyper_params.nesterov.mu = momentum_factor;
-      opt_params = {Optimizer_t::Nesterov, learning_rate, opt_hyper_params, update_type};
-      break;
-    }
-    case Optimizer_t::SGD: {
-      auto j_hparam = get_json(j_optimizer, "sgd_hparam");
-      auto learning_rate = get_value_from_json<float>(j_hparam, "learning_rate");
-      if (has_key_(j_hparam, "atomic_update")) {
-        opt_hyper_params.sgd.atomic_update = get_value_from_json<bool>(j_hparam, "atomic_update");
-      }
-      opt_params = {Optimizer_t::SGD, learning_rate, opt_hyper_params, update_type};
-      break;
-    }
-    default:
-      assert(!"Error: no such optimizer && should never get here!");
-  }
-  return opt_params;
-}
-
 template <typename T>
 static std::shared_ptr<Regularizer<T>> create_regularizer(
     const nlohmann::json& j, const Tensor2<float>& weight_buff, const Tensor2<T>& wgrad_buff,
@@ -993,7 +923,7 @@ std::unique_ptr<Network> Network::create_network(
 
   // create optimizer
   if (!inference_flag) {
-    auto opt_param = get_optimizer_param<float>(j_optimizer);
+    auto opt_param = get_optimizer_param<float>()(j_optimizer);
 
     network->optimizer_ = std::move(Optimizer::Create(
         opt_param, weight_buff->as_tensor(), wgrad_buff->as_tensor(), wgrad_buff_half->as_tensor(),
@@ -1049,9 +979,9 @@ static void create_embedding(std::map<std::string, SparseInput<TypeKey>>& sparse
 
   OptParams<TypeFP> embedding_opt_params;
   if (has_key_(j_layers, "optimizer")) {
-    embedding_opt_params = get_optimizer_param<TypeFP>(get_json(j_layers, "optimizer"));
+    embedding_opt_params = get_optimizer_param<TypeFP>()(get_json(j_layers, "optimizer"));
   } else {
-    embedding_opt_params = get_optimizer_param<TypeFP>(j_optimizer);
+    embedding_opt_params = get_optimizer_param<TypeFP>()(j_optimizer);
   }
   embedding_opt_params.scaler = scaler;
 
