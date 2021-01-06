2958daf5070c3b03fe378e6a400fe247c9fd228c
aleliu <aleliu@nvidia.com>
2021-01-06 19:39:56 +0800

    add stream in inferenc session for lookup/update gpu cache

--- HugeCTR/include/inference/session_inference.hpp
+++ HugeCTR/include/inference/session_inference.hpp
@@ -34,6 +34,8 @@ class InferenceSession : public HugeCTRModel {
   Parser parser_;
   InferenceParser inference_parser_;
   std::vector<size_t> embedding_table_slot_size;
+  std::vector<cudaStream_t> lookup_streams_;
+  std::vector<cudaStream_t> update_streams_;
 
   std::vector<std::shared_ptr<Tensor2<int>>> row_ptrs_tensors_; // embedding input row
   std::vector<std::shared_ptr<Tensor2<float>>> embedding_features_tensors_; // embedding input value vector
