3cc057967eb5c7993e75b1176b59ec405dbd8847
Fan Yu <fayu@nvidia.com>
2021-10-26 22:39:18 +0800

    Implement Update and Dump API in GPU cache. Add tests for new APIs. Correctness and performance are good.

--- gpu_cache/test/cache_op_sol_test.cu
+++ gpu_cache/test/cache_op_sol_test.cu
@@ -22,6 +22,48 @@
 #include <nv_gpu_cache.hpp>
 #include <omp.h>
 
+// The key generator
+template<typename T, typename set_hasher = MurmurHash3_32<T>>
+class KeyGenerator {
+public:
+    KeyGenerator(): gen_(rd_()) {}
+    KeyGenerator(T min, T max): gen_(rd_()), distribution_(min, max) {}
+
+    void fill_unique(T* data, size_t keys_per_set, size_t num_of_set, T empty_value) {
+        if (keys_per_set == 0 || num_of_set == 0) {
+            return;
+        }
+        assert(distribution_.max() - distribution_.min() >= keys_per_set * num_of_set);
+
+        std::unordered_set<T> set;
+        std::vector<size_t> set_sz(num_of_set, 0);
+        size_t sz = 0;
+        while (sz < keys_per_set * num_of_set) {
+            T x = distribution_(gen_);
+            if(x == empty_value){
+                continue;
+            }
+            auto res = set.insert(x);
+            if(res.second){
+                size_t src_set = set_hasher::hash(x) % num_of_set;
+                if(set_sz[src_set] < keys_per_set){
+                    data[src_set * keys_per_set + set_sz[src_set]] = x;
+                    set_sz[src_set]++;
+                    sz++;
+                }
+            }
+        }
+        assert(sz == keys_per_set * num_of_set);
+        for(size_t i = 0; i < num_of_set; i++){
+            assert(set_sz[i] == keys_per_set);
+        }
+    }
+
+private:
+  std::random_device rd_;
+  std::mt19937 gen_;
+  std::uniform_int_distribution<T> distribution_;
+};
 // The random number generator
 template<typename T>
 class IntGenerator {
@@ -121,6 +163,28 @@ void check_result(const KeyType* keys, const float* vals, size_t embedding_vec_s
     }
 }
 
+// Compare two sequence of keys and check whether they are the same(but with different order)
+template<typename KeyType>
+void compare_key(const KeyType* sequence_a, const KeyType* sequence_b, size_t len){
+    // Temp buffers for sorting
+    KeyType* sequence_a_copy = (KeyType*)malloc(len * sizeof(KeyType));
+    KeyType* sequence_b_copy = (KeyType*)malloc(len * sizeof(KeyType));
+    // Copy data to temp buffers
+    memcpy(sequence_a_copy, sequence_a, len * sizeof(KeyType));
+    memcpy(sequence_b_copy, sequence_b, len * sizeof(KeyType));
+    // Sort both arrays
+    std::sort(sequence_a_copy, sequence_a_copy + len);
+    std::sort(sequence_b_copy, sequence_b_copy + len);
+ 
+    // Linearly compare elements
+    for(size_t i = 0; i < len; i++){
+        assert(sequence_a_copy[i] == sequence_b_copy[i]);
+    }
+    // Free temp buffers
+    free(sequence_a_copy);
+    free(sequence_b_copy);
+}
+
 /* Timing funtion */
 double W_time(){
     timeval marker;
@@ -167,11 +231,12 @@ int main(int argc, char **argv) {
 
     // For random unique keys generation 
     IntGenerator<key_type> gen_key;
-    float increase = 0.1;
+    float increase = 0.1f;
 
+    // Start 1st test
     std::cout << "****************************************" << std::endl;
     std::cout << "****************************************" << std::endl;
-    std::cout << "Start Single-GPU Thread-safe Cache test " << std::endl;
+    std::cout << "Start Single-GPU Thread-safe Query and Replace API test " << std::endl;
 
     // Timimg variables
     double time_a;
@@ -361,8 +426,171 @@ int main(int argc, char **argv) {
     CUDA_CHECK(cudaFree(d_missing_len));
 
 }
-    
+
+    // 1st test Clean-up
     free(h_keys);
     delete cache;
+
+    // Start 2nd test
+    std::cout << "****************************************" << std::endl;
+    std::cout << "****************************************" << std::endl;
+    std::cout << "Start Single-GPU Thread-safe Update and Dump API test " << std::endl;
+    
+    // The key and value buffer that contains all the keys and values to be inserted into the cache
+    h_keys = (key_type*)malloc(SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type));
+    h_vals = (float*)malloc(SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float));
+    float* h_new_vals = (float*)malloc(SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float));
+
+    // The cache object to be tested
+    cache = new Cache_(cache_capacity_in_set, embedding_vec_size);
+
+    // Key generator
+    KeyGenerator<key_type> cache_key_gen;
+    // Generating random unique keys
+    cache_key_gen.fill_unique(h_keys, SLAB_SIZE * SET_ASSOCIATIVITY, cache_capacity_in_set, empty_key);
+    // Filling values vector according to the keys
+    fill_vec(h_keys, h_vals, embedding_vec_size, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set, increase);
+    // Set new value
+    increase = 1.0f;
+    // Filling values vector according to the keys
+    fill_vec(h_keys, h_new_vals, embedding_vec_size, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set, increase);
+
+    // Host-side buffers
+    // Buffers holding keys and values to be inserted, each time insert 1 slab to every slabset
+    key_type* h_insert_keys;
+    float* h_insert_vals;
+    // Buffers holding keys and values dumped and retrieved from the cache
+    key_type* h_dump_keys;
+    float* h_vals_retrieved;
+    size_t h_dump_counter;
+    size_t h_missing_len;
+    key_type* h_acc_keys;
+    
+    // Device-side buffers
+    key_type* d_keys;
+    float* d_vals;
+    // Buffers holding keys and values to be inserted, each time insert 1 slab to every slabset
+    key_type* d_insert_keys;
+    float* d_insert_vals;
+    // Buffers holding keys and values dumped and retrieved from the cache
+    key_type* d_dump_keys;
+    float* d_vals_retrieved;
+    size_t* d_dump_counter;
+    uint64_t* d_missing_index;
+    key_type* d_missing_keys;
+    size_t* d_missing_len;
+
+    CUDA_CHECK(cudaHostAlloc((void**)&h_insert_keys, SLAB_SIZE * cache_capacity_in_set * sizeof(key_type), cudaHostAllocPortable));
+    CUDA_CHECK(cudaHostAlloc((void**)&h_insert_vals, SLAB_SIZE * cache_capacity_in_set * embedding_vec_size * sizeof(float), cudaHostAllocPortable));
+    CUDA_CHECK(cudaHostAlloc((void**)&h_dump_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type), cudaHostAllocPortable));
+    CUDA_CHECK(cudaHostAlloc((void**)&h_vals_retrieved, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float), cudaHostAllocPortable));
+    CUDA_CHECK(cudaHostAlloc((void**)&h_acc_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type), cudaHostAllocPortable));
+
+    CUDA_CHECK(cudaMalloc((void**)&d_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type)));
+    CUDA_CHECK(cudaMalloc((void**)&d_vals, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float)));
+    CUDA_CHECK(cudaMalloc((void**)&d_insert_keys, SLAB_SIZE * cache_capacity_in_set * sizeof(key_type)));
+    CUDA_CHECK(cudaMalloc((void**)&d_insert_vals, SLAB_SIZE * cache_capacity_in_set * embedding_vec_size * sizeof(float)));
+    CUDA_CHECK(cudaMalloc((void**)&d_dump_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type)));
+    CUDA_CHECK(cudaMalloc((void**)&d_vals_retrieved, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float)));
+    CUDA_CHECK(cudaMalloc((void**)&d_dump_counter, sizeof(size_t)));
+    CUDA_CHECK(cudaMalloc((void**)&d_missing_index, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(uint64_t)));
+    CUDA_CHECK(cudaMalloc((void**)&d_missing_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type)));
+    CUDA_CHECK(cudaMalloc((void**)&d_missing_len, sizeof(size_t)));
+
+    // CUDA stream
+    cudaStream_t stream;
+    CUDA_CHECK(cudaStreamCreate(&stream));
+
+    // Copy all keys and values from host to device
+    CUDA_CHECK(cudaMemcpyAsync(d_keys, h_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * sizeof(key_type), cudaMemcpyHostToDevice, stream));
+    CUDA_CHECK(cudaMemcpyAsync(d_vals, h_new_vals, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set * embedding_vec_size * sizeof(float), cudaMemcpyHostToDevice, stream));
+    // Wait for stream to complete
+    CUDA_CHECK(cudaStreamSynchronize(stream));
+
+    // Each time insert 1 slab per slabset into the cache and check result
+    for(size_t i = 0; i < SET_ASSOCIATIVITY; i++){
+        // Prepare the keys and values to be inserted
+        for(size_t j = 0; j < cache_capacity_in_set; j++){
+            memcpy(h_insert_keys + j * SLAB_SIZE, h_keys + j * SLAB_SIZE * SET_ASSOCIATIVITY + i * SLAB_SIZE, SLAB_SIZE * sizeof(key_type));
+            memcpy(h_insert_vals + j * SLAB_SIZE * embedding_vec_size, h_vals + (j * SLAB_SIZE * SET_ASSOCIATIVITY + i * SLAB_SIZE) * embedding_vec_size, SLAB_SIZE * embedding_vec_size * sizeof(float));
+        }
+        // Copy the selected keys to accumulate buffer
+        memcpy(h_acc_keys + SLAB_SIZE * cache_capacity_in_set * i, h_insert_keys, SLAB_SIZE * cache_capacity_in_set * sizeof(key_type));
+
+        // Copy the <k,v> pairs from host to device
+        CUDA_CHECK(cudaMemcpyAsync(d_insert_keys, h_insert_keys, SLAB_SIZE * cache_capacity_in_set * sizeof(key_type), cudaMemcpyHostToDevice, stream));
+        CUDA_CHECK(cudaMemcpyAsync(d_insert_vals, h_insert_vals, SLAB_SIZE * cache_capacity_in_set * embedding_vec_size * sizeof(float), cudaMemcpyHostToDevice, stream));
+        // Insert the <k,v> pairs into the cache
+        cache -> Replace(d_insert_keys, SLAB_SIZE * cache_capacity_in_set, d_insert_vals, stream);
+        // Wait for stream to complete 
+        CUDA_CHECK(cudaStreamSynchronize(stream));
+
+        // Record time
+        time_a = W_time();
+        // Update the new values to the cache(including missing keys)
+        cache -> Update(d_keys, SLAB_SIZE * SET_ASSOCIATIVITY * cache_capacity_in_set, d_vals, stream, SLAB_SIZE);
+        // Wait for stream to complete 
+        CUDA_CHECK(cudaStreamSynchronize(stream));
+        // Elapsed wall time
+        time_b = W_time() - time_a;
+        printf("The Elapsed time for %zu round update is: %f sec.\n", i, time_b);
+
+        // Record time
+        time_a = W_time();
+        // Dump the keys from the cache
+        cache -> Dump(d_dump_keys, d_dump_counter, 0, cache_capacity_in_set, stream);
+        // Wait for stream to complete 
+        CUDA_CHECK(cudaStreamSynchronize(stream));
+        // Elapsed wall time
+        time_b = W_time() - time_a;
+        printf("The Elapsed time for %zu round dump is: %f sec.\n", i, time_b);
+
+        // Copy the dump counter from device to host
+        CUDA_CHECK(cudaMemcpyAsync(&h_dump_counter, d_dump_counter, sizeof(size_t), cudaMemcpyDeviceToHost, stream));
+        // Wait for stream to complete
+        CUDA_CHECK(cudaStreamSynchronize(stream));
+        // Check the dump counter
+        assert(h_dump_counter == SLAB_SIZE * cache_capacity_in_set * (i + 1));
+        // Query all the dumped keys from the cache
+        cache -> Query(d_dump_keys, h_dump_counter, d_vals_retrieved, d_missing_index, d_missing_keys, d_missing_len, stream);
+        // Copy result from device to host
+        CUDA_CHECK(cudaMemcpyAsync(h_dump_keys, d_dump_keys, h_dump_counter * sizeof(key_type), cudaMemcpyDeviceToHost, stream));
+        CUDA_CHECK(cudaMemcpyAsync(h_vals_retrieved, d_vals_retrieved, h_dump_counter * embedding_vec_size * sizeof(float), cudaMemcpyDeviceToHost, stream));
+        CUDA_CHECK(cudaMemcpyAsync(&h_missing_len, d_missing_len, sizeof(size_t), cudaMemcpyDeviceToHost, stream));
+        // Wait for stream to complete
+        CUDA_CHECK(cudaStreamSynchronize(stream));
+        // Check result
+        assert(h_missing_len == 0);
+        compare_key(h_dump_keys, h_acc_keys, h_dump_counter);
+        check_result(h_dump_keys, h_vals_retrieved, embedding_vec_size, h_dump_counter, increase);
+    }
+
+    printf("Update and Dump API test all finished!\n");
+
+    // 2nd test clean-up
+    CUDA_CHECK(cudaStreamDestroy(stream));
+    free(h_keys);
+    free(h_vals);
+    free(h_new_vals);
+
+    CUDA_CHECK(cudaFreeHost(h_insert_keys));
+    CUDA_CHECK(cudaFreeHost(h_insert_vals));
+    CUDA_CHECK(cudaFreeHost(h_dump_keys));
+    CUDA_CHECK(cudaFreeHost(h_vals_retrieved));
+    CUDA_CHECK(cudaFreeHost(h_acc_keys));
+
+    CUDA_CHECK(cudaFree(d_keys));
+    CUDA_CHECK(cudaFree(d_vals));
+    CUDA_CHECK(cudaFree(d_insert_keys));
+    CUDA_CHECK(cudaFree(d_insert_vals));
+    CUDA_CHECK(cudaFree(d_dump_keys));
+    CUDA_CHECK(cudaFree(d_vals_retrieved));
+    CUDA_CHECK(cudaFree(d_dump_counter));
+    CUDA_CHECK(cudaFree(d_missing_index));
+    CUDA_CHECK(cudaFree(d_missing_keys));
+    CUDA_CHECK(cudaFree(d_missing_len));
+
+    delete cache;
+
     return 0;
 }
