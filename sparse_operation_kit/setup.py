7ac5890f9ef9c6e559341725353eaede4c4d92dc
jianbingd <jianbingd@nvidia.com>
2021-11-16 04:04:13 +0000

    update setup.pu

--- sparse_operation_kit/setup.py
+++ sparse_operation_kit/setup.py
@@ -35,6 +35,21 @@ class SOKExtension(Extension):
     def __init__(self, name, cmake_file_path="./", sources=[], **kwargs):
         Extension.__init__(self, name, sources=sources, **kwargs)
         self._CMakeLists_dir = os.path.abspath(cmake_file_path)
+        self._sok_lib_name = "libsparse_operation_kit.so"
+        self._sok_unit_test_name = "libsok_unit_test.so"
+        self._sok_compat_ops_name = "libsparse_operation_kit_compat_ops.so"
+
+    @property
+    def sok_lib_name(self):
+        return self._sok_lib_name
+
+    @property
+    def sok_unit_test_name(self):
+        return self._sok_unit_test_name
+
+    @property
+    def sok_compat_ops_name(self):
+        return self._sok_compat_ops_name
 
 class SOKBuildExtension(build_ext):
     def build_extensions(self):
@@ -43,7 +58,8 @@ class SOKBuildExtension(build_ext):
             return
         
         gpu_capabilities = ["70", "75", "80"]
-        if os.getenv("SOK_COMPILE_SM"):
+        if os.getenv("SOK_COMPILE_GPU_SM"):
+            gpu_capabilities = os.getenv("SOK_COMPILE_GPU_SM")
             gpu_capabilities = str(gpu_capabilities).strip().split(";")
 
         use_nvtx = "OFF"
@@ -52,22 +68,20 @@ class SOKBuildExtension(build_ext):
 
         dedicated_cuda_stream = "ON"
         if os.getenv("SOK_COMPILE_ASYNC"):
-            dedicated_cuda_stream = "ON" if os.getenv("SOK_COMPILE_USE_NVTX") in ["1", "ON", "On", "on"] else "OFF"
+            dedicated_cuda_stream = "ON" if os.getenv("SOK_COMPILE_ASYNC") in ["1", "ON", "On", "on"] else "OFF"
 
         cmake_args = ["-DSM='{}'".format(";".join(gpu_capabilities)),
                       "-DUSE_NVTX={}".format(use_nvtx),
                       "-DSOK_ASYNC={}".format(dedicated_cuda_stream)]
         cmake_args = " ".join(cmake_args)
 
-        build_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), "build")
+        build_dir = self.get_ext_fullpath(self.extensions[0].name).replace(
+                        self.get_ext_filename(self.extensions[0].name), "")
+        build_dir = os.path.abspath(build_dir)
         if not os.path.exists(build_dir):
             os.makedirs(build_dir)
 
         for extension in self.extensions:
-            # ext_fullname = self.get_ext_fullname(extension.name)
-            # ext_fullpath = self.get_ext_fullpath(extension.name)
-            # print("[INFO]: ext_fullname = ", ext_fullname)
-            # print("[INFO]: ext_fullpath = ", ext_fullpath)
             try:
                 subprocess.check_call("cmake {} {} && make -j && make install".format(cmake_args, 
                                                                     extension._CMakeLists_dir), 
@@ -81,12 +95,53 @@ class SOKBuildExtension(build_ext):
             self.extensions = [ext for ext in self.extensions
                                 if os.path.exists(self.get_ext_fullpath(ext.name))]
 
+    def copy_extensions_to_source(self):
+        build_py = self.get_finalized_command("build_py")
+        for ext in self.extensions:
+            build_dir = os.path.abspath(os.path.join(self.build_lib, "lib"))
+            
+            fullname = self.get_ext_fullname(ext.name)
+            filename = self.get_ext_filename(fullname)
+            modpath = fullname.split(".")
+            package = ".".join(modpath[:-1])
+            package_dir = build_py.get_package_dir(package)
+
+            lib_name = ext.sok_lib_name
+            unit_test_name = ext.sok_unit_test_name
+            compat_ops_name = ext.sok_compat_ops_name
+
+            for filename in (lib_name, unit_test_name, compat_ops_name):
+                src_filename = os.path.join(build_dir, filename)
+                dest_filename = os.path.join(package_dir, os.path.basename(filename))
+                
+                # Always copy, even if source is older than destination, to ensure
+                # that the right extensions for the current Python/platform are used.
+                from setuptools._distutils.file_util import copy_file
+                copy_file(src_filename, dest_filename, verbose=self.verbose,
+                          dry_run=self.dry_run)
+
+            if ext._needs_stub:
+                self.write_stub(package_dir or os.curdir, ext, True)
+
+
 setup(
     name="SparseOperationKit",
     version=_GetSOKVersion(),
     author="NVIDIA",
     url="https://github.com/NVIDIA-Merlin/HugeCTR/tree/master/sparse_operation_kit",
-    extras_require={"tensorflow": "tensorflow>=1.15"}, # TODO: should use install_requires??
+    description="SparseOperationKit (SOK) is a python package wrapped GPU accelerated"
+                 " operations dedicated for sparse training / inference cases.",
+    long_description="SparseOperationKit (SOK) is a python package wrapped GPU accelerated "
+                     "operations dedicated for sparse training / inference cases. "
+                     "It is designed to be compatible with common DeepLearning (DL) frameworks, "
+                     "for instance, TensorFlow. "
+                     "Most of the algorithm implementations in SOK are extracted from HugeCTR, "
+                     "which is a GPU-accelerated recommender framework designed to distribute "
+                     "training across multiple GPUs and nodes and estimate Click-Through Rates (CTRs). "
+                     "If you are looking for a very efficient solution for CTRs, please check HugeCTR.",
+    extras_require={"tensorflow": "tensorflow>=1.15"},
+    license="Apache 2.0",
+    platforms=["Linux"],
     python_requires='>=3', # TODO: make it compatible with python2.7
     packages=find_packages(
         where="./",
@@ -94,6 +149,6 @@ setup(
         exclude=[]
     ),
     package_dir={"": "./"},
-    cmdclass={"sok_build_extension": SOKBuildExtension},
+    cmdclass={"build_ext": SOKBuildExtension},
     ext_modules=[SOKExtension("sparse_operation_kit")],
 )
