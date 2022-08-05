a645f79521de4a8d75fad39704b4994ab78f2bcd
mikemckiernan <mmckiernan@nvidia.com>
2022-08-04 20:16:14 -0400

    Remove unnecessary deps from docs build (#343)
    The sphinx_markdown_tables module isn't used.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -52,7 +52,6 @@ extensions = [
     "myst_nb",
     "sphinx_external_toc",
     "sphinx_rtd_theme",
-    "sphinx_markdown_tables",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
     "sphinx.ext.coverage",
@@ -76,7 +75,7 @@ myst_enable_extensions = [
 ]
 myst_linkify_fuzzy_links = False
 myst_heading_anchors = 4
-jupyter_execute_notebooks = "off"
+nb_execution_mode = "off"
 
 # Some documents are RST and include `.. toctree::` directives.
 suppress_warnings = ["etoc.toctree", "myst.header", "misc.highlighting_failure"]
