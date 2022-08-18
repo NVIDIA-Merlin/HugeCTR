6ac64700cc0470ae2b5cfba5cad8e5ecb6363656
Emma Qiao <qqiao@nvidia.com>
2022-08-17 21:28:13 -0700

    Split test jobs as discussion

--- test/inference/inference_model/dcn_multi_hot.py
+++ test/inference/inference_model/dcn_multi_hot.py
@@ -2,26 +2,31 @@ import hugectr
 from mpi4py import MPI
 
 solver = hugectr.CreateSolver(
-    model_name="wdl",
+    model_name="dcn",
     max_eval_batches=1,
     batchsize_eval=16384,
     batchsize=16384,
     lr=0.001,
     vvgpu=[[0]],
     repeat_dataset=True,
+    use_mixed_precision=False,
+    scaler=1.0,
+    use_cuda_graph=True,
+    metrics_spec={hugectr.MetricsType.AUC: 1.0},
 )
 reader = hugectr.DataReaderParams(
     data_reader_type=hugectr.DataReaderType_t.Norm,
-    source=["./wdl_data/file_list.txt"],
-    eval_source="./wdl_data/file_list_test.txt",
+    source=["./dcn_data/file_list.txt"],
+    eval_source="./dcn_data/file_list_test.txt",
     check_type=hugectr.Check_t.Sum,
+    num_workers=16,
 )
 optimizer = hugectr.CreateOptimizer(
     optimizer_type=hugectr.Optimizer_t.Adam,
     update_type=hugectr.Update_t.Global,
     beta1=0.9,
     beta2=0.999,
-    epsilon=0.0000001,
+    epsilon=0.0001,
 )
 model = hugectr.Model(solver, reader, optimizer)
 model.add(
@@ -30,31 +35,17 @@ model.add(
         label_name="label",
         dense_dim=13,
         dense_name="dense",
-        data_reader_sparse_param_array=[
-            hugectr.DataReaderSparseParam("wide_data", 2, True, 1),
-            hugectr.DataReaderSparseParam("deep_data", 1, True, 26),
-        ],
+        data_reader_sparse_param_array=[hugectr.DataReaderSparseParam("data1", 2, False, 26)],
     )
 )
 model.add(
     hugectr.SparseEmbedding(
         embedding_type=hugectr.Embedding_t.DistributedSlotSparseEmbeddingHash,
-        workspace_size_per_gpu_in_mb=69,
-        embedding_vec_size=1,
-        combiner="sum",
-        sparse_embedding_name="sparse_embedding2",
-        bottom_name="wide_data",
-        optimizer=optimizer,
-    )
-)
-model.add(
-    hugectr.SparseEmbedding(
-        embedding_type=hugectr.Embedding_t.DistributedSlotSparseEmbeddingHash,
-        workspace_size_per_gpu_in_mb=1074,
+        workspace_size_per_gpu_in_mb=300,
         embedding_vec_size=16,
         combiner="sum",
         sparse_embedding_name="sparse_embedding1",
-        bottom_name="deep_data",
+        bottom_name="data1",
         optimizer=optimizer,
     )
 )
@@ -68,21 +59,29 @@ model.add(
 )
 model.add(
     hugectr.DenseLayer(
-        layer_type=hugectr.Layer_t.Reshape,
-        bottom_names=["sparse_embedding2"],
-        top_names=["reshape2"],
-        leading_dim=1,
-    )
-)
-model.add(
-    hugectr.DenseLayer(
         layer_type=hugectr.Layer_t.Concat, bottom_names=["reshape1", "dense"], top_names=["concat1"]
     )
 )
 model.add(
     hugectr.DenseLayer(
-        layer_type=hugectr.Layer_t.InnerProduct,
+        layer_type=hugectr.Layer_t.Slice,
         bottom_names=["concat1"],
+        top_names=["slice11", "slice12"],
+        ranges=[(0, 429), (0, 429)],
+    )
+)
+model.add(
+    hugectr.DenseLayer(
+        layer_type=hugectr.Layer_t.MultiCross,
+        bottom_names=["slice11"],
+        top_names=["multicross1"],
+        num_layers=1,
+    )
+)
+model.add(
+    hugectr.DenseLayer(
+        layer_type=hugectr.Layer_t.InnerProduct,
+        bottom_names=["slice12"],
         top_names=["fc1"],
         num_output=1024,
     )
@@ -119,68 +118,70 @@ model.add(
 )
 model.add(
     hugectr.DenseLayer(
+        layer_type=hugectr.Layer_t.Concat,
+        bottom_names=["dropout2", "multicross1"],
+        top_names=["concat2"],
+    )
+)
+model.add(
+    hugectr.DenseLayer(
         layer_type=hugectr.Layer_t.InnerProduct,
-        bottom_names=["dropout2"],
+        bottom_names=["concat2"],
         top_names=["fc3"],
         num_output=1,
     )
 )
 model.add(
     hugectr.DenseLayer(
-        layer_type=hugectr.Layer_t.Add, bottom_names=["fc3", "reshape2"], top_names=["add1"]
-    )
-)
-model.add(
-    hugectr.DenseLayer(
         layer_type=hugectr.Layer_t.BinaryCrossEntropyLoss,
-        bottom_names=["add1", "label"],
+        bottom_names=["fc3", "label"],
         top_names=["loss"],
     )
 )
 model.compile()
 model.summary()
-model.graph_to_json(graph_config_file="/dump_infer/wdl.json")
+model.graph_to_json(graph_config_file="/dump_infer/dcn.json")
 model.fit(
-    max_iter=2300, display=200, eval_interval=2000, snapshot=2000, snapshot_prefix="/dump_infer/wdl"
+    max_iter=2300, display=200, eval_interval=2000, snapshot=2000, snapshot_prefix="/dump_infer/dcn"
 )
-model.export_predictions("/dump_infer/wdl_pred_" + str(2000), "/dump_infer/wdl_label_" + str(2000))
+model.export_predictions("/dump_infer/dcn_pred_" + str(2000), "/dump_infer/dcn_label_" + str(2000))
 
 
-from hugectr.inference import InferenceParams, CreateInferenceSession
+from hugectr.inference import InferenceModel, InferenceParams
 import numpy as np
 
 batch_size = 16384
 num_batches = 1
-data_source = "./wdl_data/file_list_test.txt"
+data_source = "./dcn_data/file_list_test.txt"
 inference_params = InferenceParams(
-    model_name="wdl",
+    model_name="dcn",
     max_batchsize=batch_size,
     hit_rate_threshold=1.0,
-    dense_model_file="/dump_infer/wdl_dense_2000.model",
-    sparse_model_files=["/dump_infer/wdl0_sparse_2000.model", "/dump_infer/wdl1_sparse_2000.model"],
+    dense_model_file="/dump_infer/dcn_dense_2000.model",
+    sparse_model_files=["/dump_infer/dcn0_sparse_2000.model"],
     device_id=0,
     use_gpu_embedding_cache=False,
     cache_size_percentage=1.0,
     i64_input_key=False,
-    use_mixed_precision=True,
+    use_mixed_precision=False,
     use_cuda_graph=True,
 )
-inference_session = CreateInferenceSession("/dump_infer/wdl.json", inference_params)
-predictions = inference_session.predict(
+inference_model = InferenceModel("/dump_infer/dcn.json", inference_params)
+predictions = inference_model.predict(
     num_batches=num_batches,
     source=data_source,
     data_reader_type=hugectr.DataReaderType_t.Norm,
     check_type=hugectr.Check_t.Sum,
 )
-grount_truth = np.loadtxt("/dump_infer/wdl_pred_2000")
-diff = predictions - grount_truth
+grount_truth = np.loadtxt("/dump_infer/dcn_pred_2000")
+diff = predictions.flatten() - grount_truth
 mse = np.mean(diff * diff)
 if mse > 1e-3:
-    raise RuntimeError("Too large mse between WDL one hot inference and training: {}".format(mse))
+    raise RuntimeError("Too large mse between DCN multi hot inference and training: {}".format(mse))
     sys.exit(1)
 else:
     print(
-        "WDL one hot inference results are consistent with those during training, mse: {}".format(
+        "DCN multi hot inference results are consistent with those during training, mse: {}".format(
             mse
         )
     )
