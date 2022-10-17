cf59c0bea1b630273160444ecceb9f8b25dadb8b
Emma Qiao <qqiao@nvidia.com>
2022-10-17 00:27:20 -0700

    Correct ci status of hdfs_backend_test and change to use computelab runner

--- ci/integration_test/inference/ps_test.sh
+++ ci/integration_test/inference/ps_test.sh
@@ -20,8 +20,11 @@ docker logs $ID
 ID=$(docker run --rm --net=host -u root -d gitlab-master.nvidia.com:5005/dl/hugectr/hugectr:devel_hps_thirdparties sh -c "export JAVA_HOME=/usr/local/jdk-16.0.2 && /usr/local/zookeeper/bin/zkServer.sh start && /usr/local/kafka/bin/kafka-server-start.sh /usr/local/kafka/config/server.properties ")
 docker logs $ID
 
-ID=$(docker run --net=host -v /mnt/nvdl/usr/aleliu/inference_ci/model_repository:/models -u root -d ${CONT} bash -cx "cd /workdir/build/bin && mkdir -p /hugectr/Test_Data/rockdb &&  ./parameter_server_test || exit 1 ")
+ID=$(docker run --gpus=all --net=host -v /home/scratch.svc_compute_arch/hugectr-ci/inference_ci/model_repository:/models -u root -d ${CONT} bash -cx "cd /workdir/build/bin && mkdir -p /hugectr/Test_Data/rockdb &&  ./parameter_server_test || exit 1 ")
 docker logs -f $ID
+exitCode=$(docker wait $ID)
+docker rm $ID
+exit $exitCode
 
 
 #docker run --rm --net=host -u root -d gitlab-master.nvidia.com:5005/dl/hugectr/hugectr:devel_hps_thirdparties sh -c "cd /usr/local/redis/7000 && ../src/redis-server redis.conf "
