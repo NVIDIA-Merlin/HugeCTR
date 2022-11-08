fcd1a8623b082350af202eb6b5c28e94f280d5ab
Michael McKiernan <mmckiernan@nvidia.com>
2022-11-07 16:48:45 -0800

    docs: Add basic SEO configuration

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -109,7 +109,8 @@ exclude_patterns = [
 #
 html_theme = "sphinx_rtd_theme"
 html_theme_options = {
-    "titles_only": True
+    "titles_only": True,
+    "analytics_id": "G-NVJ1Y1YJHK",
 }
 html_show_sourcelink = False
 
@@ -156,6 +157,7 @@ intersphinx_mapping = {
 }
 
 html_sidebars = {"**": ["versions.html"]}
+html_baseurl = "https://nvidia-merlin.github.io/HugeCTR/main"
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
