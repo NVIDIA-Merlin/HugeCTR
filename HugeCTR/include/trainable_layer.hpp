02477e1941d566bb55174effe07eb24743a34c94
Minseok Lee <minseokl@nvidia.com>
2022-11-17 19:16:22 -0800

    TrainableLayer Refactoring

--- HugeCTR/include/trainable_layer.hpp
+++ HugeCTR/include/trainable_layer.hpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021, NVIDIA CORPORATION.
+ * Copyright (c) 2022, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,43 +16,106 @@
 
 #pragma once
 
+#include <cassert>
+#include <type_traits>
+
 #include "HugeCTR/include/layer.hpp"
 
 namespace HugeCTR {
 /**
  * @brief
- * Definition of a trainable layer class.
+ * Trainable layer is the common parent of all layers with weights
+ * @tparams DType the data type of inputs, outputs, and weights
+ * @tparams use_FP32_weight if specified, the weight data type is in FP32, not DType
  */
-template <typename T>
+template <typename DType, bool use_FP32_weight = std::is_same<DType, float>::value>
 class TrainableLayer : public Layer {
- private:
-  Tensors2<float> master_weights_;
+  // FP32 input/output but lower precision weight don't make much sense.
+  static_assert(!(std::is_same<DType, float>::value && use_FP32_weight == false));
 
  protected:
-  /*
-   * stores the weight tensors of this layer.
-   */
-  Tensors2<T> weights_;
+  // Why WeightType is protected?
+  // it is convenient for a child trainable to access the weight type,
+  // especially if it wants to use FP32 weights but inputs/outputs the lower precision data.
+  // A typical example is when DType is __half but use_FP32_weight is true.
+  // Then, the child class should define the following alias to make thier code cleaner:
+  // (1) using Base = TrainableLayer<DType, true>;
+  // (2) using WeightType = typename Base::WeightType;
+  // If  useFP32_weight is false, the aliases are not necessary.
+  using WeightType = typename std::conditional<use_FP32_weight, float, DType>::type;
 
-  Tensors2<T> wgrads_;
-
+ private:
+  Tensors2<float> master_weights_;
+  Tensors2<WeightType> weights_;
+  Tensors2<WeightType> wgrads_;
   const std::shared_ptr<BufferBlock2<float>> master_weight_buff_;
-  const std::shared_ptr<BufferBlock2<T>> weight_buff_;
-  const std::shared_ptr<BufferBlock2<T>> wgrad_buff_;
+  const std::shared_ptr<BufferBlock2<WeightType>> weight_buff_;
+  const std::shared_ptr<BufferBlock2<WeightType>> wgrad_buff_;
 
-  void set_weight(size_t idx, const std::vector<size_t>& dimensions);
-  void set_wgrad(size_t idx, const std::vector<size_t>& dimensions);
-  Tensor2<T>& get_weight(size_t idx);
-  Tensor2<T>& get_wgrad(size_t idx);
+ protected:
+  // @brief a modifier to reserve a weight tensor at idx with the specified dims.
+  // @details
+  // Usage: In a child class, this->set_weight(0, dims);
+  void set_weight(size_t idx, const std::vector<size_t>& dimensions) {
+    HCTR_CHECK_HINT(weights_.size() == idx, "Wrong index for setting weight tensors");
+
+    Tensor2<WeightType> tensor;
+    weight_buff_->reserve(dimensions, &tensor);
+    weights_.push_back(tensor);
+
+    // master weights are used only when compute weights have lower precision
+    if constexpr (!use_FP32_weight) {
+      HCTR_CHECK_HINT(master_weights_.size() == idx,
+                      "Wrong index for setting master weight tensors");
+
+      Tensor2<float> tensor;
+      master_weight_buff_->reserve(dimensions, &tensor);
+      master_weights_.push_back(tensor);
+    }
+  }
+  // @brief a modifier to reserve a weight tensor at idx with the specified dims.
+  // @details
+  // Usage: In a child class, this->set_wgrad(0, dims);
+  void set_wgrad(size_t idx, const std::vector<size_t>& dimensions) {
+    HCTR_CHECK_HINT(wgrads_.size() == idx, "Wrong index for setting weight gradient tensors");
+
+    Tensor2<WeightType> tensor;
+    wgrad_buff_->reserve(dimensions, &tensor);
+    wgrads_.push_back(tensor);
+  }
+  // @brief an accessor to get a weight tensor at idx
+  // @details
+  // Usage: In a child class, auto weight2 = this->get_weight(2);
+  auto& get_weight(size_t idx) const {
+    HCTR_CHECK_HINT(idx < weights_.size(), "Wrong index for getting weight tensors");
+    return weights_[idx];
+  }
+  // @brief an accessor to get a wgrad tensor at idx
+  // @details
+  // Usage: In a child class, auto wgrad2 = this->get_wgrad(2);
+  auto& get_wgrad(size_t idx) const {
+    HCTR_CHECK_HINT(idx < wgrads_.size(), "Wrong index for getting weight gradient tensors");
+    return wgrads_[idx];
+  }
 
  public:
+  /**
+   * Ctor of TrainableLayer.
+   * @param master_weight_buff the buffer to reserve master weight tensors, used only if WeightType
+   * is not FP32.
+   * @param weight_buff the buffer to reserve weight tensors
+   * @param wgrad_buff the buffer to reserve weight gradient tensors
+   * @param gpu_resource the abstraction of GPU where this dense layer resides
+   * @param initializer_types the list of initializer types of all weight tensors
+   */
   TrainableLayer(const std::shared_ptr<BufferBlock2<float>>& master_weight_buff,
-                 const std::shared_ptr<BufferBlock2<T>>& weight_buff,
-                 const std::shared_ptr<BufferBlock2<T>>& wgrad_buff,
+                 const std::shared_ptr<BufferBlock2<WeightType>>& weight_buff,
+                 const std::shared_ptr<BufferBlock2<WeightType>>& wgrad_buff,
                  const std::shared_ptr<GPUResource>& gpu_resource,
                  std::vector<Initializer_t> initializer_types = std::vector<Initializer_t>())
       : Layer(gpu_resource, initializer_types),
-        master_weight_buff_(master_weight_buff),
+        // if WeightType is float, master weights are not used at all
+        master_weight_buff_(std::is_same<WeightType, float>::value ? nullptr : master_weight_buff),
         weight_buff_(weight_buff),
         wgrad_buff_(wgrad_buff) {}
 };
--- test/utest/layers/trainable_layer_test.cpp
+++ test/utest/layers/trainable_layer_test.cpp
@@ -20,113 +20,108 @@
 using namespace HugeCTR;
 
 namespace {
+
 /**
  * @brief
  * This is a dummy trainable layer class to demonstrate how to reserve weights and weight
  * gradients for trainable layers. The fprop and bprop methods are not actually implemented.
  */
-template <typename T>
-class DummyTrainableLayer : public TrainableLayer<T> {
-  Tensors2<T> in_tensors_;
+template <typename DType, bool use_FP32_weight>
+class DummyTrainableLayer : public TrainableLayer<DType, use_FP32_weight> {
+  using Base = TrainableLayer<DType, use_FP32_weight>;
+  using WeightType = typename Base::WeightType;
 
-  Tensors2<T> out_tensors_;
+  Tensors2<DType> in_tensors_;
+
+  Tensors2<DType> out_tensors_;
 
  public:
   DummyTrainableLayer(const std::shared_ptr<BufferBlock2<float>>& master_weight_buff,
-                      const std::shared_ptr<BufferBlock2<T>>& weight_buff,
-                      const std::shared_ptr<BufferBlock2<T>>& wgrad_buff,
-                      const Tensor2<T>& in_tensor, const Tensor2<T>& out_tensor,
+                      const std::shared_ptr<BufferBlock2<WeightType>>& weight_buff,
+                      const std::shared_ptr<BufferBlock2<WeightType>>& wgrad_buff,
+                      const Tensor2<DType>& in_tensor, const Tensor2<DType>& out_tensor,
                       const std::shared_ptr<GPUResource>& gpu_resource,
-                      std::vector<Initializer_t> initializer_types = std::vector<Initializer_t>());
+                      std::vector<Initializer_t> initializer_types = std::vector<Initializer_t>())
+      : Base(master_weight_buff, weight_buff, wgrad_buff, gpu_resource, initializer_types) {
+    const auto& in_tensor_dim = in_tensor.get_dimensions();
+    const auto& out_tensor_dim = out_tensor.get_dimensions();
 
-  void fprop(bool is_train) override{};
+    if (in_tensor_dim.size() != 2 || in_tensor_dim.size() != 2 ||
+        in_tensor_dim[0] != out_tensor_dim[0]) {
+      HCTR_OWN_THROW(Error_t::WrongInput, "Wrong dimensions for input and output tensors");
+    }
 
-  void bprop() override{};
-};
+    size_t input_size = in_tensor_dim[in_tensor_dim.size() - 1];
+    size_t output_size = out_tensor_dim[out_tensor_dim.size() - 1];
 
-template <typename T>
-DummyTrainableLayer<T>::DummyTrainableLayer(
-    const std::shared_ptr<BufferBlock2<float>>& master_weight_buff,
-    const std::shared_ptr<BufferBlock2<T>>& weight_buff,
-    const std::shared_ptr<BufferBlock2<T>>& wgrad_buff, const Tensor2<T>& in_tensor,
-    const Tensor2<T>& out_tensor, const std::shared_ptr<GPUResource>& gpu_resource,
-    std::vector<Initializer_t> initializer_types)
-    : TrainableLayer<T>(master_weight_buff, weight_buff, wgrad_buff, gpu_resource,
-                        initializer_types) {
-  const auto& in_tensor_dim = in_tensor.get_dimensions();
-  const auto& out_tensor_dim = out_tensor.get_dimensions();
+    std::vector<size_t> dim0 = {input_size, output_size};
+    std::vector<size_t> dim1 = {1, output_size};
 
-  if (in_tensor_dim.size() != 2 || in_tensor_dim.size() != 2 ||
-      in_tensor_dim[0] != out_tensor_dim[0]) {
-    HCTR_OWN_THROW(Error_t::WrongInput, "Wrong dimensions for input and output tensors");
+    this->set_weight(0, dim0);
+    this->set_weight(1, dim1);
+    this->set_wgrad(0, dim0);
+    this->set_wgrad(1, dim1);
+
+    in_tensors_.push_back(in_tensor);
+    out_tensors_.push_back(out_tensor);
   }
 
-  size_t input_size = in_tensor_dim[in_tensor_dim.size() - 1];
-  size_t output_size = out_tensor_dim[out_tensor_dim.size() - 1];
+  void fprop(bool is_train) override {
+    auto weight_0 = this->get_weight(0);
+    auto weight_1 = this->get_weight(1);
+  };
 
-  std::vector<size_t> dim0 = {input_size, output_size};
-  std::vector<size_t> dim1 = {1, output_size};
+  void bprop() override {
+    auto wgrad_0 = this->get_wgrad(0);
+    auto wgrad_1 = this->get_wgrad(1);
+  };
+};
 
-  this->set_weight(0, dim0);
-  this->set_weight(1, dim1);
-  this->set_wgrad(0, dim0);
-  this->set_wgrad(1, dim1);
-
-  in_tensors_.push_back(in_tensor);
-  out_tensors_.push_back(out_tensor);
-}
-
-template <typename T>
+template <typename DType, bool use_FP32_weight>
 void trainable_layer_test(size_t batch_size, size_t in_dim, size_t out_dim) {
   std::shared_ptr<GeneralBuffer2<CudaAllocator>> buf = GeneralBuffer2<CudaAllocator>::create();
-  std::shared_ptr<BufferBlock2<float>> master_weight_buff = buf->create_block<float>();
-  std::shared_ptr<BufferBlock2<T>> weight_buff = buf->create_block<T>();
-  std::shared_ptr<BufferBlock2<T>> wgrad_buff = buf->create_block<T>();
 
-  Tensor2<T> input_tensor;
+  using WeightType = typename std::conditional<use_FP32_weight, float, DType>::type;
+
+  std::shared_ptr<BufferBlock2<WeightType>> weight_buff = buf->create_block<WeightType>();
+  std::shared_ptr<BufferBlock2<WeightType>> wgrad_buff = buf->create_block<WeightType>();
+
+  Tensor2<DType> input_tensor;
   buf->reserve({batch_size, in_dim}, &input_tensor);
-  Tensor2<T> output_tensor;
+  Tensor2<DType> output_tensor;
   buf->reserve({batch_size, out_dim}, &output_tensor);
 
-  DummyTrainableLayer<T> dummy_trainable_layer(master_weight_buff, weight_buff, wgrad_buff,
-                                               input_tensor, output_tensor,
-                                               test::get_default_gpu());
+  auto test_impl = [&buf, &weight_buff, &wgrad_buff, &input_tensor,
+                    &output_tensor](auto master_weight_buff) {
+    DummyTrainableLayer<DType, use_FP32_weight> dummy_trainable_layer(
+        master_weight_buff, weight_buff, wgrad_buff, input_tensor, output_tensor,
+        test::get_default_gpu());
+    buf->allocate();
+    dummy_trainable_layer.initialize();
+    dummy_trainable_layer.fprop(true);
+    dummy_trainable_layer.bprop();
+  };
 
-  buf->allocate();
-  dummy_trainable_layer.initialize();
-  dummy_trainable_layer.fprop(true);
-  dummy_trainable_layer.bprop();
+  if constexpr (std::is_same<DType, float>::value || use_FP32_weight) {
+    test_impl(weight_buff);
 
-  ASSERT_TRUE(master_weight_buff->as_tensor().get_num_elements() ==
-              weight_buff->as_tensor().get_num_elements());
-  ASSERT_TRUE(master_weight_buff->as_tensor().get_num_elements() ==
-              wgrad_buff->as_tensor().get_num_elements());
-}
+    ASSERT_TRUE(weight_buff->as_tensor().get_num_elements() ==
+                wgrad_buff->as_tensor().get_num_elements());
+  } else {
+    std::shared_ptr<BufferBlock2<float>> master_weight_buff = buf->create_block<float>();
+    test_impl(master_weight_buff);
 
-template <>
-void trainable_layer_test<float>(size_t batch_size, size_t in_dim, size_t out_dim) {
-  std::shared_ptr<GeneralBuffer2<CudaAllocator>> buf = GeneralBuffer2<CudaAllocator>::create();
-  std::shared_ptr<BufferBlock2<float>> weight_buff = buf->create_block<float>();
-  std::shared_ptr<BufferBlock2<float>> wgrad_buff = buf->create_block<float>();
-
-  Tensor2<float> input_tensor;
-  buf->reserve({batch_size, in_dim}, &input_tensor);
-  Tensor2<float> output_tensor;
-  buf->reserve({batch_size, out_dim}, &output_tensor);
-
-  DummyTrainableLayer<float> dummy_trainable_layer(
-      weight_buff, weight_buff, wgrad_buff, input_tensor, output_tensor, test::get_default_gpu());
-
-  buf->allocate();
-  dummy_trainable_layer.initialize();
-  dummy_trainable_layer.fprop(true);
-  dummy_trainable_layer.bprop();
-
-  ASSERT_TRUE(weight_buff->as_tensor().get_num_elements() ==
-              wgrad_buff->as_tensor().get_num_elements());
+    ASSERT_TRUE(master_weight_buff->as_tensor().get_num_elements() ==
+                weight_buff->as_tensor().get_num_elements());
+    ASSERT_TRUE(master_weight_buff->as_tensor().get_num_elements() ==
+                wgrad_buff->as_tensor().get_num_elements());
+  }
 }
 
 }  // namespace
 
-TEST(trainable_layer, fp32_4096x1024x10) { trainable_layer_test<float>(4096, 1024, 10); }
-TEST(trainable_layer, fp16_4096x1024x10) { trainable_layer_test<__half>(4096, 1024, 10); }
+TEST(trainable_layer, fp32_32_4096x1024x10) { trainable_layer_test<float, true>(4096, 1024, 10); }
+// TEST(trainable_layer, fp32_16_4096x1024x10) { trainable_layer_test<float, false>(4096, 1024, 10);
+// }  // error
+TEST(trainable_layer, fp16_16_4096x1024x10) { trainable_layer_test<__half, false>(4096, 1024, 10); }
+TEST(trainable_layer, fp16_32_4096x1024x10) { trainable_layer_test<__half, true>(4096, 1024, 10); }
